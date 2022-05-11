7076f2e9cf3b5ad9afeb3f6b8de3df94f0d4701b
daaaaak <daaaaak2107@yandex.ru>
2022-05-11 18:24:18 +0300

    bldc fixed

--- Robot/Drivers/BLDC.py
+++ Robot/Drivers/BLDC.py
@@ -9,14 +9,15 @@ class BLDC:
         self.pin = int(pin)
         GPIO.setup(self.pin, GPIO.OUT)
         self.pwm = GPIO.PWM(self.pin, 50)
-        self.pwm.start(0.0)
+        self.pwm.start(7.0)
     
     def move(self, speed):
-        c_speed = ((speed - 127.0) / 18.2)
         if speed == 127:
-            self.pwm.ChangeDutyCycle(0.0)
+            self.pwm.ChangeDutyCycle(7.0)
+        elif speed > 127:
+            self.pwm.ChangeDutyCycle(7.0 + speed * 4.0 / 127)
         else:
-            self.pwm.ChangeDutyCycle(4 + c_speed)
+            self.pwm.ChangeDutyCycle(7.0 - speed * 4.0 / 127)
         
     def __exit__(self, exc_type, exc_val, exc_tb):
         GPIO.cleanup()
